8f0dc5707cbbed12962af7f83b1af8df58182115
Mattew03 <mattew.guerrero@epn.edu.ec>
2024-08-10 02:29:24 -0500

    Funcional, faltan detalles

--- backend/model.py
+++ backend/model.py
@@ -5,20 +5,20 @@ from tensorflow.keras.preprocessing import image # type: ignore
 import numpy as np
 
 # Cargar el modelo
-model = load_model('backend/modelo1.h5')
+model = load_model('backend/modelo2.h5')
 
 # Cargar el índice k-NN
-knn_index_path = 'D:\\U\\7. Septimo\\RI\\Proyecto-IIB-RI\\modelo\\knn_index1.pkl'
+knn_index_path = 'D:\\U\\7. Septimo\\RI\\Proyecto-IIB-RI\\modelo\\knn_index2.pkl'
 with open(knn_index_path, 'rb') as f:
     index_data = pickle.load(f)
 
 # Extraer el índice k-NN y las rutas de las imágenes
-knn_index = index_data['knn']
+knn_index = index_data
 with open(r'D:\\U\\7. Septimo\\RI\\Proyecto-IIB-RI\\backend\\train_labels_cate.pkl', 'rb') as f:
         train_image_paths = pickle.load(f)
 
 def extract_features(image_path):
-    img = image.load_img(image_path, target_size=(256, 256))
+    img = image.load_img(image_path, target_size=(224, 224))
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)
     img_array /= 255.0  # Normalización, ajusta según tu modelo
